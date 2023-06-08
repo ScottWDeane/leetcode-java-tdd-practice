61ffdbb0783c49513550fb9e9b9e7693b1956787
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-08 14:17:53 -0700

    Update FizzBuzz.java
    first test pass: calcFizzBuzz now returns a string (currently empty).

--- demo/src/main/java/tdd/FizzBuzz.java
+++ demo/src/main/java/tdd/FizzBuzz.java
@@ -7,5 +7,8 @@ package tdd;
 public class FizzBuzz {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+    public static String calcFizzBuzz(int i) {
+        return "";
     }
 }
