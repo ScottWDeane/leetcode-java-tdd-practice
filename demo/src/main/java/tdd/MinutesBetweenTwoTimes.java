b39e74e0bb3a1f48b026048116ac8323bdcac2ca
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-08 15:49:05 -0700

    create files for test
    create main java class and test class for Minutes Between Two Times puzzle.

---
+++ demo/src/main/java/tdd/MinutesBetweenTwoTimes.java
@@ -0,0 +1,8 @@
+package tdd;
+
+public class MinutesBetweenTwoTimes {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ demo/src/test/java/tdd/MinutesBetweenTest.java
@@ -0,0 +1,11 @@
+package tdd;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MinutesBetweenTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
