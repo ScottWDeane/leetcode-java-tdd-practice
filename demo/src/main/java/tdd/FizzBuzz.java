fc81f5723cf14a44d2836a09373f0b8c8a4e6a8f
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-08 14:38:30 -0700

    Update FizzBuzz.java
    fix test: multiples of 3 and 5 return "FizzBuzz". All previous tests are green.

--- demo/src/main/java/tdd/FizzBuzz.java
+++ demo/src/main/java/tdd/FizzBuzz.java
@@ -10,7 +10,9 @@ public class FizzBuzz {
     }
 
     public static String calcFizzBuzz(int i) {
-        if (i % 3 == 0) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        } else if (i % 3 == 0) {
             return "Fizz";
         } else if (i % 5 == 0) {
             return "Buzz";
