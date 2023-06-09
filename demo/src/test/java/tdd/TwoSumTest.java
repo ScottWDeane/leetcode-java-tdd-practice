d8cd50404aef94af3b36d81fc0fcf1c567a651f6
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-09 16:54:59 -0700

    extract magic number
    extract magic numbers, rename variables to be more specific

--- demo/src/test/java/tdd/TwoSumTest.java
+++ demo/src/test/java/tdd/TwoSumTest.java
@@ -11,9 +11,11 @@ public class TwoSumTest {
     }
 
     @Test
-    public void whenGiven_NumArray_2_7_11_15_withTarget9_returnIndices_0_1() {
-        int[] intArray = {1, 2, 3};
-        int[] indices = {0, 1};
-        assertArrayEquals(TwoSum.twoSum(intArray, 9), indices);
+    public void whenGiven_NumArray_2_7_11_15_withTarget_9_returnIndices_0_1() {
+        int[] intArray = {2, 7, 11, 15};
+        int target = 9;
+        int[] expectedIndices = {0, 1};
+        assertArrayEquals(TwoSum.twoSum(intArray, target), expectedIndices);
+    }
     }
 }
