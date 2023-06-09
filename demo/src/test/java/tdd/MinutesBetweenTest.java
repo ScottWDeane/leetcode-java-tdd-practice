b3b252b09249229749a9b30eef2acb12b9fe325b
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-08 19:43:59 -0700

    Update MinutesBetweenTest.java
    add missing semicolons
    re-name a few tests to be more specific (it's not "EU Time Fifteen", it should be "EU Time Fifteen OClock"

--- demo/src/test/java/tdd/MinutesBetweenTest.java
+++ demo/src/test/java/tdd/MinutesBetweenTest.java
@@ -8,33 +8,33 @@ public class MinutesBetweenTest {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
-    }
+    };
 
     @Test
     public void whenCalledWithString_shouldReturnInteger() {
         assertTrue(MinutesBetweenTwoTimes.calcDifference("anyString") instanceof Integer);
-    }
+    };
 
     @Test
     public void whenExtractFirstTime_withTenAmToElevenAm_shouldExtractUpToFirstCharacters() {
         assertTrue(StringUtils.equals(MinutesBetweenTwoTimes.extractFirstTime("10:00am-11:00am"),
                 "10:00am"));
-    }
+    };
 
     @Test
     public void whenExtractFirstTime_withElevenAmToTwelvePm_shouldReturnString11AM() {
         assertTrue(StringUtils.equals(MinutesBetweenTwoTimes.extractFirstTime("11:00am-12:00pm"),
                 "11:00am"));
-    }
+    };
 
     @Test
-    public void whenConvertToEUTime_withThreePM_shouldConvertToEUTimeFifteen() {
+    public void whenConvertToEUTime_withThreePM_shouldConvertToEUTimeFifteenOClock() {
         assertTrue(StringUtils.equals(MinutesBetweenTwoTimes.convertToEUTime("3:00pm"), "15:00"));
-    }
+    };
 
     @Test
-    public void whenConvertToEUTime_withEightPM_shouldConvertToEUTimeTwenty() {
+    public void whenConvertToEUTime_withEightPM_shouldConvertToEUTimeTwentyOClock() {
         assertTrue(StringUtils.equals(MinutesBetweenTwoTimes.convertToEUTime("8:00pm"), "20:00"));
-    }
+    };
 
 }
