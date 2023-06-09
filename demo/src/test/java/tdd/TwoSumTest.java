593b9dfe7a26fa8d2248eca05b45437709403373
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-09 16:36:07 -0700

    init commit
    init commit, sanity test to make sure test java class can run.

---
+++ demo/src/test/java/tdd/TwoSumTest.java
@@ -0,0 +1,11 @@
+package tdd;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TwoSumTest {
+    @Test
+    public void test_checker() {
+        assertTrue(true);
+    }
+
