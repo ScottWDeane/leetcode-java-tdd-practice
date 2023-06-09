596be55e92798ac51a7c4e44d589a270814e613a
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-09 16:50:25 -0700

    Update TwoSumTest.java
    first failing test: when twoSum() is given int array {2,7,11,15} with target 9, return indices (as an int array) {0,1}

--- demo/src/test/java/tdd/TwoSumTest.java
+++ demo/src/test/java/tdd/TwoSumTest.java
@@ -9,3 +9,10 @@ public class TwoSumTest {
         assertTrue(true);
     }
 
+    @Test
+    public void whenGiven_NumArray_2_7_11_15_withTarget9_returnIndices_0_1() {
+        int[] intArray = {1, 2, 3};
+        int[] indices = {0, 1};
+        assertArrayEquals(TwoSum.twoSum(intArray, 9), indices);
+    }
+}
