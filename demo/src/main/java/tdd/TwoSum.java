36c9c0af9b7eddc38daca8f4ed125a874c176166
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-09 21:14:22 -0700

    Update TwoSum.java
    fix failing test: inner loop now skips its own index rather than its own value.

--- demo/src/main/java/tdd/TwoSum.java
+++ demo/src/main/java/tdd/TwoSum.java
@@ -5,7 +5,7 @@ public class TwoSum {
     public static int[] twoSum(int[] intArray, int target) {
         int[] sumIndices = new int[2];
         for (int i = 0; i < intArray.length; i++) {
-            for (int y = 0; y < intArray.length && intArray[y] != intArray[i]; y++) {
+            for (int y = 0; y < intArray.length && y != i; y++) {
                 if (intArray[i] + intArray[y] == target) {
                     sumIndices[1] = i;
                     sumIndices[0] = y;
