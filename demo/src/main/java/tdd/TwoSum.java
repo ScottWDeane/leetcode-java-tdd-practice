e0e0847bcaf4e8eaa7f2bce87bb1cc2b8966b034
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-09 21:09:32 -0700

    Update TwoSum.java
    fix failing test: when passed int array {3,2,4} and target 6, returns array with indices {1,2}

--- demo/src/main/java/tdd/TwoSum.java
+++ demo/src/main/java/tdd/TwoSum.java
@@ -3,7 +3,16 @@ package tdd;
 public class TwoSum {
 
     public static int[] twoSum(int[] intArray, int target) {
-        int[] sumIndices = {0, 1};
+        int[] sumIndices = new int[2];
+        for (int i = 0; i < intArray.length; i++) {
+            for (int y = 0; y < intArray.length && intArray[y] != intArray[i]; y++) {
+                if (intArray[i] + intArray[y] == target) {
+                    sumIndices[1] = i;
+                    sumIndices[0] = y;
+                    return sumIndices;
+                }
+            }
+        }
         return sumIndices;
     }
 
